95c85449934f5bdca61f32c9a5f3d30d98cfaf5c
Vladimir <Vladimir@VLADIMIRPC>
2025-10-13 23:34:07 +0700

    Add project files.

---
+++ SAPR/Abstractions/Building/IWeightPlateBuilder.cs
@@ -0,0 +1,26 @@
+﻿using WeightPlatePlugin.Abstractions.Core;
+
+namespace WeightPlatePlugin.Abstractions.Building
+{
+    /// <summary>Построитель модели блина (сценарий высокого уровня).</summary>
+    public interface IWeightPlateBuilder
+    {
+        /// <summary>Полное построение модели по параметрам.</summary>
+        void Build(IWeightPlateParameters parameters);
+
+        /// <summary>Создать 2D-профиль для операции вращения.</summary>
+        void BuildProfile(IWeightPlateParameters parameters);
+
+        /// <summary>Вращение профиля в тело.</summary>
+        void RevolvePlate(IWeightPlateParameters parameters);
+
+        /// <summary>Создание центрального отверстия (Ø d).</summary>
+        void MakeCenterHole(IWeightPlateParameters parameters);
+
+        /// <summary>Создание внутреннего углубления (диаметр L, глубина G).</summary>
+        void MakeRecess(IWeightPlateParameters parameters);
+
+        /// <summary>Применение фасок/скруглений по кромкам (R).</summary>
+        void ApplyFillets(IWeightPlateParameters parameters);
+    }
+}
---
+++ SAPR/Abstractions/Core/IValidator.cs
@@ -0,0 +1,8 @@
+﻿namespace WeightPlatePlugin.Abstractions.Core
+{
+    /// <summary>Общий контракт валидатора.</summary>
+    public interface IValidator<T>
+    {
+        IValidationResult Validate(T value);
+    }
+}
